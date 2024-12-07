feb98bcde5688a967ae97caf465eb0308ec9449e
Vanessa Roque <viroque19@gmail.com>
2024-12-07 18:41:28 +0000

    day 5 pt1

--- day5/part2.py
+++ day5/part2.py
@@ -16,18 +16,23 @@ def main(input):
     with open(input, 'r') as f:
         lines = f.readlines()
     
+    # Split rules and updates
     idx = lines.index("\n")
     rules = lines[:idx]
     updates = lines[idx+1:]
 
+    # Initialize rules_map
     rules_map = defaultdict(list)
     for line in rules:
         line = line.strip()
         pre, post = line.split('|')
         rules_map[pre].append(post)
 
+    # Process updates and calculate result
     res = 0
     for pages in updates:
+        if not pages.strip():
+            continue  # Skip empty lines
         pages = [x.strip() for x in pages.split(',')]
         if isValid(rules_map, pages):
             mid = int(pages[len(pages) // 2])
